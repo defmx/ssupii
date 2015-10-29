222ad2cf9cebdef3bfd93adaafafafa16422f49b
ivsvadm <dxmessage@gmail.com>
2015-10-29 12:47:22 -0600

    ng-src

--- assets/js/upiifx.js
+++ assets/js/upiifx.js
@@ -1,13 +1,18 @@
-(function(){
-	var dz=angular.module('upii',[]);
-	dz.controller('CTRL', ['$scope','$http', function($scope,$http){
-	$http.get('http://ssupii.azurewebsites.net/z/s').
-		success(function(data, status, headers, config) {
-			data='{"a":"a","sch":'+data+'}';
-			var a=JSON.parse(data);
-			$scope.data=a;
-		}).
-		error(function(data, status, headers, config) {
-		});
-	}]);
-})();
+ (function(){
+  var dz=angular.module('upii',[]);
+  dz.controller('CTRL', ['$scope','$http', function($scope,$http){
+                         var ctrl=this;
+                         $http.get('http://ssupii.azurewebsites.net/z/s').
+                         success(function(data, status, headers, config) {
+                                 data='{"a":"a","sch":'+data+'}';
+                                 var a=JSON.parse(data);
+                                 $scope.data=a;
+                                 }).
+                         error(function(data, status, headers, config) {
+                               });
+                         ctrl.bOnClick=function(text){
+                         $scope.title=text;
+                         }
+                         
+                         }]);
+  })();
