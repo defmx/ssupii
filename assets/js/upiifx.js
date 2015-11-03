802ab29fdeb28a3f82e14838e76e8ab6e4524497
ivsvadm <dxmessage@gmail.com>
2015-11-03 12:48:30 -0600

    h

--- assets/js/upiifx.js
+++ assets/js/upiifx.js
@@ -2,17 +2,45 @@
   var dz=angular.module('upii',[]);
   dz.controller('CTRL', ['$scope','$http', function($scope,$http){
                          var ctrl=this;
-                         $http.get('http://ssupii.azurewebsites.net/z/s').
+                         $scope.title="";
+                         $scope.currSem="14 - 2";
+                         $http.get('http://ssupii.azurewebsites.net/z/ls?q=sch').
                          success(function(data, status, headers, config) {
                                  data='{"a":"a","sch":'+data+'}';
+                                 var a=JSON.parse(data);
+                                 $scope.data1=a;
+                                 }).
+                         error(function(data, status, headers, config) {
+                               });
+                         $http.get('http://ssupii.azurewebsites.net/z/ls?q=sem').
+                         success(function(data, status, headers, config) {
+                                 data='{"a":"a","sem":'+data+'}';
+                                 var a=JSON.parse(data);
+                                 $scope.data2=a;
+                                 }).
+                         error(function(data, status, headers, config) {
+                               });
+                         ctrl.bOnClick=function(id,name){
+                         $scope.title=name;
+                         $http.get('http://ssupii.azurewebsites.net/z/ls?q=alumn&w='+id).
+                         success(function(data, status, headers, config) {
+                                 data='{"a":"a","alumn":'+data+'}';
+                                 var a=JSON.parse(data);
+                                 $scope.data3=a;
+                                 }).
+                         error(function(data, status, headers, config) {
+                               });
+                         }
+                         ctrl.semOnClick=function(y,s){
+                         $scope.currSem=' '+y+' - '+s;
+                         $http.get('http://ssupii.azurewebsites.net/z/ls?q=alumn&sem='+y+'-'+s).
+                         success(function(data, status, headers, config) {
+                                 data='{"a":"a","sem":'+data+'}';
                                  var a=JSON.parse(data);
                                  $scope.data=a;
                                  }).
                          error(function(data, status, headers, config) {
                                });
-                         ctrl.bOnClick=function(text){
-                         $scope.title=text;
                          }
-                         
                          }]);
   })();
